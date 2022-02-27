6e4fe1933200b809f4ad97133474f1e1363dacc1
JOY-SAM <joysam1111@gmail.com>
2022-02-27 17:51:00 +0530

    Waiting for Evaluation of Proccessor.js

--- src/utlis/proccessor.js
+++ src/utlis/proccessor.js
@@ -1,35 +1,186 @@
+let majorFact='You are 18,'
+let nonMajorFact='You are under 18,'
+let loanFact=''
+let nonLoanFact=''
+let employedFact=''
+let nonEmployedFact=''
+let financialInstruments=''
+let nonFinancialInstrumentsFact=''
+let familyFact=''
+let nonFamilyFact=''
+let insuranceFact=''
+let nonInsurancesFact=''
+let carFact=''
+let nonCarFact=''
+let incomeFact=''
+let nonIncomeFact=''
+let houseFact=''
+let nonHouseFact=''
 
-
-let formInputs = {
-    age : 18,
-    name : 'nameadsdasd'
-}
-
-export const processAge = (formInputs)=> {
-    console.log(formInputs)
-    if(parseInt(formInputs.age) >= 18) {
-        return {
-            'description' : 'You are 18, some fact',
-            type : 'AGE'
+class Person {
+    constructor(props)
+    {
+        console.log(props)
+        const {age,employed,loan,financialInstruments,family,insurance,car,income,house} = props
+    }
+    isMajor() {
+        if(this.age>=18)
+        {
+            return true
+        }
+      return false
+    }
+    processAge(){
+        if(this.isMajor()) {
+            return {
+                'description' : majorFact,
+                type : 'AGE'
+            }
+        }
+        else{
+            return {
+                'description' : nonMajorFact,
+                type : 'AGE'
+            }
         }
     }
-    else if(parseInt(formInputs.age) < 18) {
-        return {
-            'description' : 'You are under 18, some fact',
-            type : 'AGE'
+    isEmployed()
+    {
+        if(this.employed === true) {
+            return true
+        }
+        return false
+    }
+    processEmployed()
+    {
+        if(isEmployed()) {
+            return {
+                'description' : employedFact,
+                type : 'Employed'
+            }
+        }
+        else{
+            return {
+                'description' : nonEmployedFact,
+                type : 'Employed'
+            }
         }
     }
+    processLoan()
+    {
+        if(this.loan === true) {
+            return {
+                'description' : loanFact,
+                type : 'Loan'
+            }
+        }
+        else{
+            return {
+                'description' : nonLoanFact,
+                type : 'Loan'
+            }
+        }
+    }
+    
+    processFinancialInstruments()
+    {
+        if(this.employed === true) {
+            return {
+                'description' : financialInstruments,
+                type : 'Finanical'
+            }
+        }
+        else{
+            return {
+                'description' : nonFinancialInstrumentsFact,
+                type : 'Finanical'
+            }
+        }
+    }
+    processFamily()
+    {
+        if(this.family === true) {
+            return {
+                'description' : familyFact,
+                type : 'Family'
+            }
+        }
+        else{
+            return {
+                'description' : nonFamilyFact,
+                type : 'Family'
+            }
+        }
+    }
+    processInsurance()
+    {
+        if(this.insurance === true) {
+            return {
+                'description' : insuranceFact,
+                type : 'Family'
+            }
+        }
+        else{
+            return {
+                'description' : nonInsurancesFact,
+                type : 'finanical'
+            }
+        }
+    }
+    processCar()
+    {
+        if(this.car === true) {
+            return {
+                'description' : carFact,
+                type : 'Car'
+            }
+        }
+        else{
+            return {
+                'description' : nonCarFact,
+                type : 'Car'
+            }
+        }
+    }
+    processIncome()
+    {
+        if(this.income === true) {
+            return {
+                'description' : incomeFact,
+                type : 'Income'
+            }
+        }
+        else{
+            return {
+                'description' : nonIncomeFact,
+                type : 'Income'
+            }
+        }
+    }
+    processHouse()
+    {
+        if(this.house === true) {
+            return {
+                'description' : houseFact,
+                type : 'House'
+            }
+        }
+        else{
+            return {
+                'description' : nonHouseFact,
+                type : 'House'
+            }
+        }
+    }
+  }
+  
+
+
+
+export const processObject = (formInputs) =>{
+    const obj = new Person(formInputs);
+    return obj
 }
-
-
-
-// export const processAge = (proos.age) =>{
-//     if(props.age < 18)
-//     {
-//         return "minor ";
-//     }
-//     else if ()
-// }
 
 // export const policies = (props.policiy) => {
 
